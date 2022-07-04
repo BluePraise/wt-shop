c7b3ad6fd1a6050e24deec9122132452e6577245
Magalie Chetrit <email@magaliechetrit.com>
2022-07-04 16:35:11 -0400

    dynamic header count

--- header.php
+++ header.php
@@ -5,7 +5,14 @@
 			<li><a href="/">WT Shop</a></li>
 			<li><a href="/">(Random)</a></li>
 			<li><a href="/">(List)</a></li>
-			<li><a href="/">Cart</a></li>
+			<li>
+				<a href="/">Cart <span>(<?php echo WC()->cart->get_cart_contents_count(); ?>)</span></a>
+				<?php if (WC()->cart->get_cart_contents_count() == 0 ) : ?>
+					<div class="mini-cart-column hide">
+						<div class="mini-cart-total"><?php woocommerce_mini_cart(); ?></div>
+					</div>
+				<?php endif; ?>
+			</li>
 		</ul>
 	</nav>
 </header>
